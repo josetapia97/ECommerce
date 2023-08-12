cfb2b2e7f114b33fee7e4276b98f1bbd9abaf641
JoseTapiaJara <coteloxx@gmail.com>
2023-08-12 19:31:57 -0400

    Modelamiento y coneccion bd

---
+++ Ecommerce.Repositorio/Implementacion/GenericoRepositorio.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.Repositorio.Contrato;
+using Ecommerce.Repositorio.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Repositorio.Implementacion
+{
+    public class GenericoRepositorio<Tmodelo> : IGenericoRepositorio<Tmodelo> where Tmodelo : class
+    {
+        private readonly DbecomerceContext _dbContext;
+        public GenericoRepositorio(DbecomerceContext dbContext )
+        {
+            _dbContext = dbContext;
+        }
+        public IQueryable<Tmodelo> Consultar(Expression<Func<Tmodelo, bool>>? filtro = null)
+        {
+            IQueryable<Tmodelo> consulta = (filtro == null) ? _dbContext.Set<Tmodelo>() : _dbContext.Set<Tmodelo>().Where(filtro);
+            return consulta;
+        }
+
+        public async Task<Tmodelo> Crear(Tmodelo modelo)
+        {
+            try
+            {
+                _dbContext.Set<Tmodelo>().Add(modelo);
+                await _dbContext.SaveChangesAsync();
+                return modelo;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<bool> Editar(Tmodelo modelo)
+        {
+            try
+            {
+                _dbContext.Set<Tmodelo>().Update(modelo);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<bool> Eliminar(Tmodelo modelo)
+        {
+            try
+            {
+                _dbContext.Set<Tmodelo>().Remove(modelo);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Ecommerce.Repositorio/Implementacion/VentaRepositorio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.Modelo;
+using Ecommerce.Repositorio.Contrato;
+using Ecommerce.Repositorio.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Repositorio.Implementacion
+{
+    public class VentaRepositorio : GenericoRepositorio<Venta>, IVentaRepositorio
+    {
+        private readonly DbecomerceContext _dbContext;
+        public VentaRepositorio(DbecomerceContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Venta> Registrar(Venta modelo)
+        {
+            Venta ventaGenerada = new Venta();
+            using (var transaction = _dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach(DetalleVenta dv in modelo.DetalleVenta)
+                    {
+                        //encontramos el producto
+                        Producto producto_encontrado = _dbContext.Productos.Where(p => p.IdProducto == dv.IdProducto).First();
+                        //restamos el stock
+                        producto_encontrado.Cantidad = producto_encontrado.Cantidad - dv.Cantidad;
+                        _dbContext.Productos.Update(producto_encontrado);
+                    }
+                    //una vez termina de hacer todos los pasos los guarda en la bd
+                    await _dbContext.SaveChangesAsync();
+                    await _dbContext.Venta.AddAsync(modelo);
+                    await _dbContext.SaveChangesAsync();
+
+                    ventaGenerada = modelo;
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+
+            return ventaGenerada;
+        }
+    }
+}
