a89cfa0a688b382b5c4b4290bfe751ca5efc1e7d
JoseTapiaJara <coteloxx@gmail.com>
2023-08-17 12:46:51 -0400

    Implementacion servicio carrito

--- Ecommerce.WebAssembly/Program.cs
+++ Ecommerce.WebAssembly/Program.cs
@@ -2,10 +2,29 @@ using Ecommerce.WebAssembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+//Inyectando dependencias para el carrito
+using Blazored.LocalStorage;
+//Para mensajes
+using Blazored.Toast;
+//servicio carrito
+using Ecommerce.WebAssembly.Servicios.Implementacion;
+using Ecommerce.WebAssembly.Servicios.Contrato;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5201/api/") });
+
+//Inyectando dependencias para el carrito
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredToast();
+//inyectando servicios para luego usarlos en webassembly
+builder.Services.AddScoped<IUsuarioServicio, UsuarioServicio>();
+builder.Services.AddScoped<ICategoriaServicio, CategoriaServicio>();
+builder.Services.AddScoped<IProductoServicio, ProductoServicio>();
+builder.Services.AddScoped<ICarritoServicio, CarritoServicio>();
+builder.Services.AddScoped<IVentaServicio, VentaServicio>();
+builder.Services.AddScoped<IDashboardServicio, DashboardServicio>();
 
 await builder.Build().RunAsync();
